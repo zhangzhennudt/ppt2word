fdc3c1f646a247accc97c647029ce39715f4d5ab
zhangzhennudt <34596612+zhangzhennudt@users.noreply.github.com>
2018-11-23 16:08:07 +0800

    Add files via upload

---
+++ ppt2word.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 25 12:37:48 2018
+
+@author:zhangzhennudt
+@email:zhangzhennudt@126.com
+"""
+
+import win32com
+import os
+from win32com.client import Dispatch, constants
+ppt = win32com.client.Dispatch('PowerPoint.Application')
+ppt.Visible = 1
+print("开始导出")
+pptSel = ppt.Presentations.Open("D:/codepython/导出PPT到WORD/ppt/1.pptx")
+win32com.client.gencache.EnsureDispatch('PowerPoint.Application')
+f = open("D:/codepython/导出PPT到WORD/1.txt","w")
+slide_count = pptSel.Slides.Count
+for i in range(1,slide_count + 1):
+  shape_count = pptSel.Slides(i).Shapes.Count
+  print(shape_count)
+  for j in range(1,shape_count + 1):
+    if pptSel.Slides(i).Shapes(j).HasTextFrame:
+      s = pptSel.Slides(i).Shapes(j).TextFrame.TextRange.Text
+      f.write(s+ "\n")
+f.close()
+ppt.Quit()
+print("导出完毕")
